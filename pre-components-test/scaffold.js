34c0a2ca3753ea1bdbfd765c7eb4eaf792f7a4a7
Rerurate514 <rerurateyuto@gmail.com>
2025-02-19 20:36:27 +0900

    scaffold - pre追加

---
+++ pre-components-test/scaffold.js
@@ -0,0 +1,53 @@
+import { View, Column } from "fjuttes";
+import { Header } from "./header.js";
+
+export class Scaffold extends View {
+    constructor({
+        child,
+        header = null,
+        footer = null,
+        drawer = null,
+        floatingActionButton = null,
+    }){
+        if(!(header instanceof Header)){
+            throw new Error("Scaffold header property must assign Header class");
+        }
+
+        super({
+            child: child,
+            header: header,
+            footer: footer,
+            drawer: drawer,
+            floatingActionButton: floatingActionButton,
+        });
+    }
+
+    createWrapView(){
+        return document.createElement("div");   
+    }
+
+    styledView(element){
+        element.style.width = "100%";
+        element.style.height = "100%";
+
+        return element;
+    }
+
+    build(){
+        const children = [];
+        
+        if (this.props.header) {
+            children.push(
+                this.props.header
+            );
+        }
+        
+        children.push(this.props.child);
+        
+        if (this.props.footer) {
+            children.push(this.props.footer);
+        }
+        
+        return new Column(children);
+    }
+}
