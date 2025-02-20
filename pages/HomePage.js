a610fdb8047d5aa5b96824d86155248e65c1a049
Rerurate514 <rerurateyuto@gmail.com>
2025-02-20 23:36:08 +0900

    marginを12%へ変更

---
+++ pages/HomePage.js
@@ -0,0 +1,67 @@
+import { BaseCSS, Column, FontCSS, Image, SpaceBox, Text, TextCSS, View } from "fjuttes";
+import { Scaffold } from "../pre-components-test/scaffold.js";
+import { Header } from "../pre-components-test/header.js";
+import { MyHeader } from "../widget/mHeader.js";
+import { PageMargin } from "../widget/pageMargin.js";
+
+export class HomePage extends View {
+    constructor(){
+        super();
+    }
+
+    createWrapView(){
+        return document.createElement("div");
+    }
+
+    build(){
+        return new Scaffold({
+            header: new Header({
+                isStickyHeader: true,
+                child: new MyHeader()
+            }),
+            child: new PageMargin(
+                new _HomeContent()
+            ),
+        })
+    }
+}
+
+class _HomeContent extends View {
+    constructor(){
+        super();
+    }
+
+    createWrapView(){
+        return document.createElement("div");
+    }
+
+    build(){
+        return new Column({
+            isHorizontalCenter: true,
+            children: [
+                new SpaceBox({
+                    height: "64px"
+                }),
+                new Image({
+                    src: "../src/fjuttes-official-logo.svg",
+                    baseCSS: new BaseCSS({
+                        width: "256px"
+                    })
+                }),
+                new SpaceBox({
+                    height: "16px"
+                }),
+                new Text(
+                    "fJutteS is Pure JS Components base Lib.",
+                    new TextCSS({
+                        fontCSS: new FontCSS({
+                            fontWeight: "bold",
+                            color: "#3B3B3B",
+                            fontSize: "64px"
+                        })
+                    })
+                )
+            ]
+        })
+    }
+}
