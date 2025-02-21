78f0e873ea6a25e40a90c89a275f2e740706d01b
Rerurate514 <rerurateyuto@gmail.com>
2025-02-22 00:03:23 +0900

    コンテンツの更新

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -1,4 +1,4 @@
-import { BaseCSS, Card, Column, FontCSS, Image, Margin, Grid, Text, TextCSS, BorderCSS, WebkitCSS, View, Hover, ElevatedButton, Link, SpaceBox, Padding, Center, Row, ShadowLevel, Box } from "fjuttes";
+import { BaseCSS, Card, Column, FontCSS, Image, Margin, Grid, Text, TextCSS, BorderCSS, WebkitCSS, View, Hover, Stack, Link, SpaceBox, Padding, Center, Row, ShadowLevel, Box, Position } from "fjuttes";
 import { Scaffold } from "../pre-components-test/scaffold.js";
 import { Header } from "../pre-components-test/header.js";
 import { MyHeader } from "../widget/mHeader.js";
@@ -152,29 +152,62 @@ class _HomeContent extends View {
                     baseCSS: new BaseCSS({
                         padding: "16px"
                     }),
-                    child: new Row({
-                        isVerticalCenter: true,
+                    child: new Grid({
+                        minmaxPX: 279,
                         children: [
-                            new Text(
-                                "fJutteS is a library that provides a Flutter-like writing experience in web development.",
-                                new TextCSS({
-                                    fontCSS: new FontCSS({
-                                        fontWeight: "bold",
-                                        fontSize: "20px"
+                            new Center(
+                                new Text(
+                                    "fJutteS is a library that provides a Flutter-like writing experience in web development.",
+                                    new TextCSS({
+                                        fontCSS: new FontCSS({
+                                            fontWeight: "bold",
+                                            fontSize: "20px"
+                                        })
                                     })
-                                })
+                                ),
                             ),
-                            new SpaceBox({
-                                width: "32px",
-                                height: "32px"
-                            }),
-                            new Box({
-                                width: "fit-content",
-                                height: "fit-content",
-                                child: new Image({
-                                    src: "https://raw.githubusercontent.com/Rerurate514/fJutteS-Wiki/refs/heads/main/src/flike.png"
+                            new Card({
+                                baseCSS: new BaseCSS({
+                                    margin: "16px"
+                                }),
+                                child: new Stack({
+                                    baseCSS: new BaseCSS({
+                                        width: "100%",
+                                        height: "256px"
+                                    }),
+                                    children: [
+                                        new Position({
+                                            top: 0,
+                                            left: 0,
+                                            child: new Box({
+                                                width: "100%",
+                                                height: "100%",
+                                                borderCSS: new BorderCSS({
+                                                    radius: "32px"
+                                                }),
+                                                child: new Image({
+                                                    src: "https://raw.githubusercontent.com/Rerurate514/fJutteS-Wiki/refs/heads/main/src/flike.png",
+                                                    baseCSS: new BaseCSS({
+                                                        height: "100%",
+                                                        borderCSS: new BorderCSS({
+                                                            radius: "16px"
+                                                        })
+                                                    })
+                                                }),
+                                            })
+                                        }),
+                                        new Position({
+                                            top: 0,
+                                            left: 0,
+                                            child: new Box({
+                                                width: "100%",
+                                                height: "100%",
+                                                background: "linear-gradient(to right, white 10%, transparent 70%)",
+                                            })
+                                        }),
+                                    ]
                                 })
-                            })
+                            }),
                         ]
                     })
                 })
