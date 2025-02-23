10b878586327f2fb29fa13efb43691bc23ad372a
Rerurate514 <rerurateyuto@gmail.com>
2025-02-23 21:35:53 +0900

    MyTextCSSを追加

---
+++ setting/myTextCSS.js
@@ -0,0 +1,9 @@
+import { TextCSS, FontCSS } from "fjuttes";
+
+export const SummaryText = new TextCSS({
+    fontCSS: new FontCSS({
+        fontWeight: "bold",
+        fontSize: "20px",
+        color: "white"
+    })
+});
