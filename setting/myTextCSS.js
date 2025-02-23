63a670ac329903552b95e0f9a23866f243f1a8e7
Rerurate514 <rerurateyuto@gmail.com>
2025-02-23 21:39:09 +0900

    色指定を追加

--- setting/myTextCSS.js
+++ setting/myTextCSS.js
@@ -1,9 +1,17 @@
 import { TextCSS, FontCSS } from "fjuttes";
 
-export const SUMMARY_TEXT_CSS = new TextCSS({
+export const SUMMARY_TEXT_CSS_WHITE = new TextCSS({
     fontCSS: new FontCSS({
         fontWeight: "bold",
         fontSize: "20px",
         color: "white"
+    })
+});
+
+export const SUMMARY_TEXT_CSS_BLACk = new TextCSS({
+    fontCSS: new FontCSS({
+        fontWeight: "bold",
+        fontSize: "20px",
+        color: "black"
     })
 });
