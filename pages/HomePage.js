257cfe5b32458987df5d46bfcb5c8fcf5f42c89b
Rerurate514 <rerurateyuto@gmail.com>
2025-02-23 21:57:39 +0900

    コンテンツの更新

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -4,6 +4,9 @@ import { Header } from "../pre-components-test/header.js";
 import { MyHeader } from "../widget/mHeader.js";
 import { PageMargin } from "../widget/pageMargin.js";
 import { LOGO_URL } from "../const/LOGO_URL.js";
+import { SectionTitle } from "../widget/sectionTitle.js";
+import { ColoredText } from "../widget/coloredText.js";
+import { SUMMARY_TEXT_CSS_BLACk, SUMMARY_TEXT_CSS_WHITE } from "../setting/myTextCSS.js";
 
 export class HomePage extends View {
     constructor(){
@@ -43,6 +46,10 @@ class _HomeContent extends View {
                 new SpaceBox({height: "64px"}),
                 this._buildSummary(),
                 new SpaceBox({height: "64px"}),
+                this._buildJiperesSummary(),
+                new SpaceBox({height: "64px"}),
+                this._buildWithoutNpmSummary(),
+                new SpaceBox({height: "64px"}),
             ]
         })
     }
@@ -60,26 +67,9 @@ class _HomeContent extends View {
                         })
                     }),
                 }),
-                new Card({
-                    background: "linear-gradient(135deg, #EDD51C, #29C5F8)",
-                    webkitCSS: new WebkitCSS({
-                        webkitBackgroundClip: "text",
-                    }),
-                    child: new Text(
-                        "fJutteS is Pure JavaScript Components base Lib.",
-                        new TextCSS({
-                            fontCSS: new FontCSS({
-                                fontWeight: "bold",
-                                color: "#3B3B3B",
-                                fontSize: "64px",
-                                fontFamily: "zap",
-                                fontWeight: "bold"
-                            }),
-                            webkitCSS: new WebkitCSS({
-                                webkitTextFillColor: "transparent"
-                            })
-                        })
-                    )
+                new ColoredText({
+                    text: "fJutteS is Pure JavaScript Components base Lib.",
+                    fontSize: "64px"
                 }),
                 new Card({
                     baseCSS: new BaseCSS({
@@ -156,15 +146,15 @@ class _HomeContent extends View {
                         minmaxPX: 279,
                         children: [
                             new Center(
-                                new Text(
-                                    "fJutteS is a library that provides a Flutter-like writing experience in web development.",
-                                    new TextCSS({
-                                        fontCSS: new FontCSS({
-                                            fontWeight: "bold",
-                                            fontSize: "20px"
-                                        })
-                                    })
-                                ),
+                                new Column({
+                                    children: [
+                                        new SectionTitle("SIMULAR EXPERIENCE"),
+                                        new Text(
+                                            "fJutteS is a library that provides a Flutter-like writing experience in web development.",
+                                            SUMMARY_TEXT_CSS_BLACk
+                                        ),
+                                    ]
+                                })
                             ),
                             new Card({
                                 baseCSS: new BaseCSS({
@@ -210,6 +200,79 @@ class _HomeContent extends View {
                             }),
                         ]
                     })
+                })
+            ]
+        })
+    }
+
+    _buildJiperesSummary(){
+        return new Column({
+            children: [
+                new Card({
+                    elevation: ShadowLevel.LVL5,
+                    radius: "16px",
+                    background: "#3B3B3B",
+                    baseCSS: new BaseCSS({
+                        padding: "16px"
+                    }),
+                    child: new Grid({
+                        minmaxPX: 279,
+                        children: [
+                            new Card({
+                                baseCSS: new BaseCSS({
+                                    margin: "16px",
+                                    width: "100%",
+                                    height: "256px"
+                                }),
+                                child: new Image({
+                                    src: "https://raw.githubusercontent.com/Rerurate514/fJutteS-Wiki/refs/heads/main/src/jiperes_logo.jpg",
+                                    baseCSS: new BaseCSS({
+                                        height: "100%",
+                                        borderCSS: new BorderCSS({
+                                            radius: "16px"
+                                        })
+                                    })
+                                }),
+                            }),
+                            new Center(
+                                new Column({
+                                    children: [
+                                        new SectionTitle("AUTO MANAGE STATE"),
+                                        new Text(
+                                            "The built-in state management library Jiperes makes it easy to manage widget state.",
+                                            SUMMARY_TEXT_CSS_WHITE
+                                        ),
+                                    ]
+                                })
+                            ),
+                        ]
+                    })
+                })
+            ]
+        })
+    }
+
+    _buildWithoutNpmSummary(){
+        return new Column({
+            children: [
+                new SectionTitle("MAKE WITHOUT NPM", "64px"),
+                new Text(
+                    "fJutteS can build a UI using only index.html if you use a CDN. npm installation is possible, but in that case, we recommend that you consider using other libraries.",
+                    SUMMARY_TEXT_CSS_BLACk
+                ),
+                new SpaceBox({
+                    height: "32px",
+                }), 
+                new Row({
+                    children: [
+                        new Image({
+                            baseCSS: new BaseCSS({
+                                width: "256px"
+                            }),
+                            src: "https://raw.githubusercontent.com/Rerurate514/fJutteS-Wiki/a94fea640a04db0316f048bb35c768b2cc2dc16f/src/npm-logo-red.svg"
+                        }),
+                        
+                    ]
                 })
             ]
         })
