76db57de053e0fa29895ec706d255839ec6182ee
Rerurate514 <rerurateyuto@gmail.com>
2025-03-08 18:02:04 +0900

    コード例セクション追加

--- logic/generateComponentsWiki.js
+++ logic/generateComponentsWiki.js
@@ -1,5 +1,6 @@
 import { COMPONENTS } from "../const/COMPONENTS_COL.js";
 import { COMPONENTS_USAGE_EXAMPLES_INSTANCES } from "../const/COMPONENTS_EXAMPLE.js";
+import { COMPONENTS_USAGE_EXAMPLES_INSTANCES_STRING } from "../const/COMPONENTS_EXAMPLE_CODE.js";
 import { COMPONENTS_SUMMARY_EN } from "../const/COMPONENTS_SUMMARY.js";
 import { WikiComponentTemplate } from "../widget/wikiComponentTemplate.js";
 
@@ -8,7 +9,8 @@ export function generateComponentsWiki(){
         return new WikiComponentTemplate({
             name: component,
             summary: COMPONENTS_SUMMARY_EN[component],
-            example: COMPONENTS_USAGE_EXAMPLES_INSTANCES[component]
+            example: COMPONENTS_USAGE_EXAMPLES_INSTANCES[component],
+            exampleCode: COMPONENTS_USAGE_EXAMPLES_INSTANCES_STRING[component]
         })
     });
 }
