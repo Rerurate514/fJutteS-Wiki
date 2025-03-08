e031022cafa563570db6e04e530a741c7e978e59
Rerurate514 <rerurateyuto@gmail.com>
2025-03-08 18:55:18 +0900

    フォルダの削除

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
-import { assembleView, ProviderObserver } from "fjuttes";
+import { assembleView, ProviderObserver, Scaffold, Header } from "fjuttes";
 import { routerProvider } from "./providers/routerProvider.js";
-import { Scaffold } from "./pre-components-test/scaffold.js";
-import { Header } from "./pre-components-test/header.js";
 import { MyHeader } from "./widget/mHeader.js";
 import { MyFooter } from "./widget/mFooter.js";
 
