24550614141b2ade3c35c01d607619850c71b63b
Rerurate514 <rerurateyuto@gmail.com>
2025-04-03 19:30:50 +0900

    developper情報を追加

--- pages/developpersPage.js
+++ pages/developpersPage.js
@@ -1,6 +1,7 @@
-import { View, Scaffold, Header } from "fjuttes";
+import { View, Scaffold, Header, Text, Margin, Link, Card, ShadowLevel, BaseCSS, Row, Clip, Image, Column, TextCSS, FontCSS, Center } from "fjuttes";
 import { MyHeader } from "../widget/mHeader.js";
 import { MyFooter } from "../widget/mFooter.js";
+import { PageMargin } from "../widget/pageMargin.js";
 
 export class DeveloppersPage extends View {
     constructor(){
@@ -28,11 +29,69 @@ class _DeveloppersPage extends View {
         super();
     }
 
-    createWrapView(){
-        return document.createElement("div");
-    }
-
-    build(){
-
+    build() {
+        return new PageMargin(
+            new Margin({
+                top: "64px",
+                bottom: "64px",
+                child: new Card({
+                    elevation: ShadowLevel.LVL4,
+                    radius: "16px",
+                    baseCSS: new BaseCSS({
+                        padding: "32px",
+                    }),
+                    child: new Row({
+                        baseCSS: new BaseCSS({
+                            alignItems: "center"
+                        }),
+                        children: [
+                            new Clip({
+                                child: new Image({
+                                    src: "../src/skadi_ico.png",
+                                    baseCSS: new BaseCSS({
+                                        width: "256px",
+                                        height: "256px",
+                                    })
+                                }),
+                                baseCSS: new BaseCSS({
+                                    borderRadius: "50%",
+                                    overflow: "hidden",
+                                })
+                            }),
+                            new Margin({
+                                left: "32px",
+                                child: new Center(
+                                    new Column({
+                                        children: [
+                                            new Text(
+                                                "fJutteS Developper - FJD",
+                                                new TextCSS({
+                                                    textAlign: "left",
+                                                    fontCSS: new FontCSS({
+                                                        fontSize: "25px"
+                                                    })
+                                                })
+                                            ),
+                                            new Margin({ top: "8px" }),
+                                            new Text("I'm a fJutteS Components lib developper."),
+                                            new Margin({ top: "32px" }),
+                                            new Link({
+                                                href: "https://rerurate514.github.io/portfolirate/",
+                                                child: new Text(
+                                                    "This is My portfolio.",
+                                                    new TextCSS({
+                                                        textAlign: "left"
+                                                    })
+                                                )
+                                            })
+                                        ]
+                                    })
+                                )
+                            })
+                        ],
+                    })
+                })
+            })
+        );
     }
 }
